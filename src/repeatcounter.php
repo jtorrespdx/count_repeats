53b02383e989fbecfe714d9eae0b7bcf171425c6
Jose <jtorres.pdx@gmail.com>
2015-08-14 14:03:16 -0700

    adds tests to CountRepeatTest.php that pass up to the fifth case (specs) without modifying the method since the previous commit.

--- src/repeatcounter.php
+++ src/repeatcounter.php
@@ -2,26 +2,18 @@
 
     class RepeatCounter
     {
-        function countRepeats($phrase, $word)
-        {
-            
+        function countRepeats($phrase, $word) {
+        //start our counter at 0
+        $count = 0;
+        //
+        $exploded_phrase = explode(" ", $phrase);
+            foreach ($exploded_phrase as $phrase_word) {
+                if($phrase_word == $word){
+                    ++$count;
+                }
+            }
+            return $count;
         }
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 ?>
--- tests/CountRepeatTest.php
+++ tests/CountRepeatTest.php
@@ -5,10 +5,9 @@
     class RepeatCounterTest extends PHPUnit_Framework_TestCase
     {
 
-        //CASE 1
-        function test_count_repeats()
+        //CASE 1 finding a matching word and counting it as 1.
+        function test_count_repeats_1()
         {
-
             //Arrange
             $test_CountRepeats = new RepeatCounter;
             $phrase = "a";
@@ -18,7 +17,67 @@
             $result = $test_CountRepeats->countRepeats($phrase, $word);
 
             //Assert
-            $this->assertEquals("1", $result);
+            $this->assertEquals(1, $result);
+        }
+
+        //CASE  Finds no matching words for a count of 0.
+        function test_count_repeats_2()
+        {
+            //Arrange
+            $test_CountRepeats = new RepeatCounter;
+            $phrase = "a";
+            $word = "b";
+
+            //Act
+            $result = $test_CountRepeats->countRepeats($phrase, $word);
+
+            //Assert
+            $this->assertEquals(0, $result);
+        }
+
+        //CASE 3 finds two matches and shows count of 2.
+        function test_count_repeats_3()
+        {
+            //Arrange
+            $test_CountRepeats = new RepeatCounter;
+            $phrase = "be be";
+            $word = "be";
+
+            //Act
+            $result = $test_CountRepeats->countRepeats($phrase, $word);
+
+            //Assert
+            $this->assertEquals(2, $result);
+        }
+
+        //CASE 4 finds matches with in mixed words, finds two, counts 2.
+        function test_count_repeats_4()
+        {
+            //Arrange
+            $test_CountRepeats = new RepeatCounter;
+            $phrase = "be a be";
+            $word = "be";
+
+            //Act
+            $result = $test_CountRepeats->countRepeats($phrase, $word);
+
+            //Assert
+            $this->assertEquals(2, $result);
+        }
+
+        //CASE 5 Finds words in the phrase but only counts words of exact length.
+        function test_count_repeats_5()
+        {
+            //Arrange
+            $test_CountRepeats = new RepeatCounter;
+            $phrase = "be b be bee";
+            $word = "be";
+
+            //Act
+            $result = $test_CountRepeats->countRepeats($phrase, $word);
+
+            //Assert
+            $this->assertEquals(2, $result);
         }
     }
 ?>
